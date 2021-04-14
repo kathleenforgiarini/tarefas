dd3aeb57eb63ca3b2acf7f44d62b1a60f5b31daf
Kathleen Forgiarini <kfor.silva@gmal.com>
2021-04-13 22:22:22 -0300

    Atualização de rotas para criação e visualização de tarefas; Adição de módulo body-parser; Adição de model Tarefa

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
 const express = require("express");
 const app = express();
 const handlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
+const tarefa = require("./models/Tarefa");
 
-app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 
 //Rotas
 
-app.get('/pagamento', function(req, res) {
-    res.render('pagamento');
+app.get('/tarefa', function(req, res) {
+    res.render('tarefa');
 })
 
-app.get('/add-pagamento', function(req, res) {
-    res.render('add-pagamento');
+app.get('/cad-tarefa', function(req, res) {
+    res.render('cad-tarefa');
 })
 
+app.post('/add-tarefa', function(req, res) {
+    tarefa.create({
+        nome: req.body.nome,
+        descricao: req.body.descricao
+    }).then(function() {
+        // res.send("Tarefa cadastrada com sucesso!");
+        res.redirect('/tarefa');
+    }).catch(function(erro) {
+        res.send("Erro ao cadastrar tarefa: " + erro);
+    });
+    // res.send("Nome: " + req.body.nome + "<br>Descrição: " + req.body.descricao + "<br>");
+
+})
 
 app.listen(8080);
